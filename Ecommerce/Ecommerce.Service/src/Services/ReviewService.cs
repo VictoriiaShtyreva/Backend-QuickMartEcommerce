48d0895439c2a3bb01c8e876557938074842185a
Viktoriia <vikatori1409801@gmail.com>
2024-05-14 20:30:34 +0300

    Added ReviewService

---
+++ Ecommerce/Ecommerce.Service/src/Services/ReviewService.cs
@@ -0,0 +1,58 @@
+using AutoMapper;
+using Ecommerce.Core.src.Common;
+using Ecommerce.Core.src.Entities;
+using Ecommerce.Core.src.Interfaces;
+using Ecommerce.Service.src.DTOs;
+using Ecommerce.Service.src.Interfaces;
+
+namespace Ecommerce.Service.src.Services
+{
+    public class ReviewService : BaseService<Review, ReviewReadDto, ReviewCreateDto, ReviewUpdateDto, QueryOptions>, IReviewService
+    {
+        private readonly IReviewRepository _reviewRepository;
+        private readonly IProductRepository _productRepository;
+        private readonly IUserRepository _userRepository;
+
+        public ReviewService(IReviewRepository reviewRepository, IMapper mapper, IProductRepository productRepository, IUserRepository userRepository)
+           : base(reviewRepository, mapper)
+        {
+            _reviewRepository = reviewRepository;
+            _productRepository = productRepository;
+            _userRepository = userRepository;
+        }
+
+        public async Task<IEnumerable<ReviewReadDto>> GetReviewsByProductIdAsync(Guid productId)
+        {
+            var reviews = await _reviewRepository.GetReviewsByProductIdAsync(productId);
+            return _mapper.Map<IEnumerable<ReviewReadDto>>(reviews);
+        }
+
+        public async Task<IEnumerable<ReviewReadDto>> GetReviewsByUserIdAsync(Guid userId)
+        {
+            var reviews = await _reviewRepository.GetReviewsByUserIdAsync(userId);
+            return _mapper.Map<IEnumerable<ReviewReadDto>>(reviews);
+        }
+
+        public async Task<ReviewReadDto> CreateOneAsync(Guid userId, ReviewCreateDto createDto)
+        {
+            // Validate existence of user and product using their IDs by constructing entities for existence check
+            var user = new User { Id = userId };
+            var product = new Product { Id = createDto.ProductId };
+            // Check for existence
+            var userExists = await _userRepository.ExistsAsync(user);
+            var productExists = await _productRepository.ExistsAsync(product);
+
+            if (!userExists || !productExists)
+            {
+                throw AppException.NotFound();
+            }
+            // Map DTO to Review entity, and set user and product IDs
+            var newReview = _mapper.Map<Review>(createDto);
+            newReview.UserId = userId;  // Set user ID
+            newReview.ProductId = createDto.ProductId;  // Set product ID
+            // Add new review to the repository and save changes
+            var result = await _reviewRepository.CreateAsync(newReview);
+            return _mapper.Map<ReviewReadDto>(result);
+        }
+    }
+}
